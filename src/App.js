0f2dadf2b816485268acfdf7f8c73edb566a120b
Kate <smolyarchik1100@gmail.com>
2018-07-07 19:30:22 +0300

    Ошибки сегодняшнего урока

--- src/App.js
+++ src/App.js
@@ -3,32 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
 class App extends Component {
-  render() {
+  render () {
     return (
-              <div className = "wrapper">
-                <SayFullName name = "Kate" surname = "Smolyar" link = "https://vk.com/smolyar_k" />
-                <SayFullName name = "Marina" surname = "Sadakova" link = "https://vk.com/marsadakove" />
-                <SayFullName name = "Ivan" surname = "Fomochkin" link = "https://vk.com/iwanteat0405" />
-              </div>     
+            <div className = "App">
+                <Header />
+                <Menu />
+                <Main />
+            </div>
     );
   }
 } 
-
-
-function SayFullName (props) {
-  return (
-      <div>
-      <h1> Моё имя {props.name}, фамилия - {props.surname} </h1>
-      <a href = {props.link}> Ссылка на мой профиль </a>
-      </div>  
-    )
-}
-
 
 export default App;
