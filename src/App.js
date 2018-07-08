c908e269abf834526f31c8a77b416faf53bd498d
Kate <smolyarchik1100@gmail.com>
2018-07-08 19:33:34 +0300

    2 DZ

--- src/App.js
+++ src/App.js
@@ -6,17 +6,52 @@ import Grid from 'react-bootstrap/lib/Grid';
 
 import styled from 'styled-components';
 
-import Header from './components/Header'
-import Menu from './components/Menu'
-import Main from './components/Main'
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render () {
     return (
             <div className = "App">
-                <Header />
-                <Menu />
-                <Main />
+              <HeaderWrapper>
+                  <Grid>
+                    <Header />
+                  </Grid>
+              </HeaderWrapper>
+              
+              <MenuWrapper>
+                  <Grid>
+                    <Menu />
+                  </Grid>  
+              </MenuWrapper>
+
+              <MainWrapper>
+                  <Grid>
+                    <Main />
+                  </Grid>
+              </MainWrapper>
             </div>
     );
   }
